c239530bd76fef50c635b56373924e19d1b9c50c
Cesar Gonzalez Groh <clgonzalezgroh@hotmail.com>
2020-11-18 22:40:08 -0300

    Final clase 9. Obteniendo los cursos desde mongo

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 "use strict";
+
+require("dotenv").config();
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-const { graphql, buildSchema } = require("graphql");
+const { makeExecutableSchema } = require("graphql-tools");
 const { readFileSync } = require("fs");
 const { join } = require("path");
 const resolvers = require("./lib/resolvers");
@@ -10,9 +12,11 @@ const app = express();
 const port = process.env.port || 3000;
 
 // schema definition
-const schema = buildSchema(
-  readFileSync(join(__dirname, "lib", "schema.graphql"), "utf-8")
+const typeDefs = readFileSync(
+  join(__dirname, "lib", "schema.graphql"),
+  "utf-8"
 );
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 app.use(
   "/api",
